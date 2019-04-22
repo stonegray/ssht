533f94326c1dc323a5a5e41bda0d5deeb58d877e
stonegray <stone@stonegray.ca>
2019-04-22 14:02:56 -0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+//const nodeExternals = require('webpack-node-externals');
+// if we have swnp, use it.
+let swnp = (a)=>a;
+
+module.exports = swnp({
+	target: 'node',
+	mode: 'production',
+	entry: {
+		app: ['./server.js']
+	},
+	resolve: {
+		modules: ['node_modules']
+	},
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: 'min.js'
+	}
+});
