12d7a8a4ec7542d2b146aea0ba7e3cda78346b27
stonegray <stone@stonegray.ca>
2019-04-24 22:30:29 -0400

    add proof-of-concept google cloud parser

--- src/hosts.ts
+++ src/hosts.ts
@@ -2,14 +2,17 @@
 
 import { getSshHosts } from "./sshConfig";
 import { getDockerHosts } from "./dockerConfig";
-
-
-
-
-
+import { getGcpHosts } from "./gcpConfig";
 
 export const hosts = async () => {
-let f = await getSshHosts();
-let g = await getDockerHosts();
-return f.concat(g);
+  process.stdout.write("ssht: searching Docker hosts..." + "\r");
+  let d = await getDockerHosts();
+
+  process.stdout.write("ssht: searching SSH config file hosts..." + "\r");
+  let f = await getSshHosts();
+
+  process.stdout.write("ssht: searching Google Cloud Compute hosts..." + "\r");
+  let g = await getGcpHosts();
+
+  return f.concat(g).concat(d);
 };
--- src/ui.ts
+++ src/ui.ts
@@ -4,135 +4,127 @@ import chalk from "chalk";
 import * as prompts from "prompts";
 import { hosts } from "./hosts";
 
-const commandPrefix:string = '\\';
+const commandPrefix: string = "\\";
 
 function genRow(h): string {
-	const w = process.stdout.columns;
+  const w = process.stdout.columns;
 
-	let host: string = h.fqdn;
+  let host: string = h.fqdn;
 
-	let lengthOffset: number = 0;
-	// const chalk = { gray: a => a };
+  let lengthOffset: number = 0;
+  // const chalk = { gray: a => a };
 
-	// If the device has a specified port, append it
-	if (typeof h.port !== "undefined") {
-		host += chalk.gray(":" + h.port);
-		lengthOffset -= 10;
-	}
+  // If the device has a specified port, append it
+  if (typeof h.port !== "undefined") {
+    host += chalk.gray(":" + h.port);
+    lengthOffset -= 10;
+  }
 
-	if (typeof h.username !== "undefined") {
-		host = chalk.gray(h.username + "@") + host;
-		lengthOffset -= 10;
-	}
+  if (typeof h.username !== "undefined") {
+    host = chalk.gray(h.username + "@") + host;
+    lengthOffset -= 10;
+  }
 
-	const endText = h.kind;
-	const instertAt = (str, sub, pos) =>
-		`${str.slice(0, pos)}${sub}${str.slice(pos)}`;
-	return instertAt(
-		`${h.name} (${host})`.padEnd(w + w),
-		endText,
-		w - endText.length - lengthOffset,
-	).substring(0, w - lengthOffset); // logs 'I want an apple'
+  const endText = h.kind;
+  const instertAt = (str, sub, pos) =>
+    `${str.slice(0, pos)}${sub}${str.slice(pos)}`;
+  return instertAt(
+    `${h.name} (${host})`.padEnd(w + w),
+    endText,
+    w - endText.length - lengthOffset
+  ).substring(0, w - lengthOffset); // logs 'I want an apple'
 }
 
 async function getChoices(arr) {
-	// Get hosts list:
-	const ch = [];
+  // Get hosts list:
+  const ch = [];
 
-	arr.forEach((h) => {
-		// rearrange and format hosts
+  arr.forEach(h => {
+    // rearrange and format hosts
 
-		ch.push({
-			title: genRow(h),
-			value: "" + arr.indexOf(h),
-		});
-	});
+    ch.push({
+      title: genRow(h),
+      value: "" + arr.indexOf(h)
+    });
+  });
 
-	return ch;
+  return ch;
 }
 
 // runtime commands
 const commands = [];
 commands.push({
-	str: 'foo',
-	run: ()=>{
-		console.log('ASDFASD')
-	}
-})
-
+  str: "foo",
+  run: () => {
+    console.log("ASDFASD");
+  }
+});
 
 const suggestByTitle = (input, choices) => {
-	// Figure out what the multiple searches are:
-	const searches = input.split(" ");
-	const activeCommandModes = [];
+  // Figure out what the multiple searches are:
+  const searches = input.split(" ");
+  const activeCommandModes = [];
 
-	// Check if a command is used; add it to the active list:
-	commands.forEach((c)=>{
-		if (terms.includes(commandPrefix+c.str)){
-			c.run();	
-		}
-	});
+  // Check if a command is used; add it to the active list:
+  commands.forEach(c => {
+    if (searches.includes(commandPrefix + c.str)) {
+      c.run();
+    }
+  });
 
-	// Main search filtering function:
-	const search = (i, terms:Array<string>): boolean => {
-		let doesMatch = 0;
+  // Main search filtering function:
+  const search = (i, searches): boolean => {
+    let doesMatch = 0;
 
-	
-		searches.forEach((s) => {
-			if (i.title.includes(s)) { doesMatch++; }
-		});
+    searches.forEach(s => {
+      if (i.title.includes(s)) {
+        doesMatch++;
+      }
+    });
 
-		return doesMatch >= searches.length;
-	}
+    return doesMatch >= searches.length;
+  };
 
-
-	// Return the promise
-	return Promise.resolve(
-		
-		// Filter choices by matching function:
-		choices.filter((i) => {
-			return search(i, searches);
-		}),
-	);
+  // Return the promise
+  return Promise.resolve(
+    // Filter choices by matching function:
+    choices.filter(i => {
+      return search(i, searches);
+    })
+  );
 };
-
 
 // main function:
 (async () => {
-	const arr = await hosts();
-		const availableHosts = await getChoices(arr);
+  const arr = await hosts();
+  const availableHosts = await getChoices(arr);
 
-	// Start autocomplete prompt:
-	const response = await prompts([
-		{
-			type: "autocomplete",
-			name: "index",
-			message: "Select host",
-			suggest: suggestByTitle,
-			choices: availableHosts,
-		},
-	]);
+  // Start autocomplete prompt:
+  const response = await prompts([
+    {
+      type: "autocomplete",
+      name: "index",
+      message: "Select host",
+      suggest: suggestByTitle,
+      choices: availableHosts
+    }
+  ]);
 
-	const index = response.index;
-	process.stdout.write("".padEnd(process.stdout.columns));
-	process.stdout.write("\x1B[2A");
-	process.stdout.write("".padEnd(process.stdout.columns));
-	console.log();
-	if (index == null) {
-		console.log(chalk.red("Unable to locate host"));
-		return;
-	}
-	// console.log(index, response, arr[index.toString()]);
+  const index = response.index;
+  process.stdout.write("".padEnd(process.stdout.columns));
+  process.stdout.write("\x1B[2A");
+  process.stdout.write("".padEnd(process.stdout.columns));
+  console.log();
+  if (index == null) {
+    console.log(chalk.red("Unable to locate host"));
+    return;
+  }
+  // console.log(index, response, arr[index.toString()]);
 
-	const target = arr[index.toString()];
+  const target = arr[index.toString()];
 
-	const cp = require("child_process");
-	const childProcess = cp.spawn("ssh", [target.ssh], { stdio: "inherit" });
+  const cp = require("child_process");
+  const childProcess = cp.spawn("ssh", [target.ssh], { stdio: "inherit" });
 })();
-
-
-
-
-
 
 //
