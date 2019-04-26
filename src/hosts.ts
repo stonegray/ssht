01765cf425b84f10629dcdf5454cba60a6df4181
stonegray <stone@stonegray.ca>
2019-04-25 21:09:25 -0400

    remove hosts.ts to avoid errors

--- src/hosts.ts
+++ src/hosts.ts
@@ -1,19 +1,21 @@
 // ssht
 
-import { getSshHosts } from './discover/ssh';
-import { getDockerHosts } from './discover/docker';
-import { getGcpHosts } from './discover/gcp';
+//import { getSshHosts } from './discover/ssh';
+//import { getDockerHosts } from './discover/docker';
+//import { getGcpHosts } from './discover/gcp';
+
 
 export const hosts = async () => {
 	process.stdout.write('ssht: searching Docker hosts...' + '\r');
-	let d = await getDockerHosts();
+	//let d = await getDockerHosts();
 
 	process.stdout.write('ssht: searching SSH config file hosts...' + '\r');
-	let f = await getSshHosts();
+	//let f = await getSshHosts();
 
 	process.stdout.write('ssht: searching Google Cloud Compute hosts...' + '\r');
-	let g = await getGcpHosts();
+	//let g = await getGcpHosts();
 
 	//@ts-ignore
-	return f.concat(g).concat(d);
+	//return f.concat(g).concat(d);
 };
+
