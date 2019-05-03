45c8ab668ed6a11a9d9113c98b47c72c65c304d2
stonegray <stone@stonegray.ca>
2019-05-03 07:14:15 -0400

    handle some errors in post selection

--- src/ui.ts
+++ src/ui.ts
@@ -3,8 +3,10 @@
 import chalk from 'chalk';
 import * as prompts from 'prompts';
 import * as cp		from 'child_process';
-import {  DSPool } from './pool';
+import { DSPool } from './pool';
 
+
+// single backtick; escaped
 const commandPrefix: string = '\\';
 
 // initialize host pool
@@ -15,6 +17,15 @@ function genRow(h): string {
 
 	let host: string = h.fqdn;
 
+
+	/* When insert color codes into the string, they're nonpriting, but take up
+	 * a number of characters within the string. So, for a temporary hack to
+	 * prevent line length issues, we just decrement counter whenever we use it,
+	 * and this counter is an offset when we build the final line string.
+	 *
+	 * Some (line.len > process.stdout.width) sorta thing might also not be a
+	 * bad idea
+	 */
 	let lengthOffset: number = 0;
 	// const chalk = { gray: a => a };
 
@@ -29,7 +40,7 @@ function genRow(h): string {
 		lengthOffset -= 10;
 	}
 
-	const endText = '' + h.kind;
+	const endText = '' + ( h.kind || '' );
 	const instertAt = (str, sub, pos) => {
 		return `${str.slice(0, pos)}${sub}${str.slice(pos)}`;
 	};
@@ -72,13 +83,20 @@ const suggestByTitle = (input, choices) => {
 		}
 	}
 
+
+
 	// Figure out what the multiple searches are:
 	const searches = input.split(' ');
 	// Main search filtering function:
 	const search = (i, searches): boolean => {
+
 		let doesMatch = 0;
 
 		searches.forEach(s => {
+			// Modify search before comparing:
+			s = s.replace(/`/, '');
+
+
 			if (typeof i.title === 'undefined') return;
 			if (i.title.includes(s)) {
 				doesMatch++;
@@ -138,14 +156,41 @@ function genMatchArray(arr){
 	console.log();
 
 	if (index == null) {
-		console.log(chalk.red('Unable to locate host'));
+		console.log(chalk.red('Unable to locate that host.'));
+		process.exit(1);
 		return;
+		
 	}
 
-	console.error(index, index.toString(), pool.hosts[index.toString()]);
-	// console.log(index, response, arr[index.toString()]);
+	if (index > pool.hosts.length ){
+		console.error(chalk.red('Internal database failure. Index out of bounds.'));
+		console.error('Please report this error on the issue tracker.');
+		process.exit(2);
+	}
+	
+	// get the record that the user requested
+	const target = pool.hosts[index];
+	
+	// Get plugin name for debugging:
+	let p: string = 'Unknown';
+	if (typeof target === 'undefined'){
+		console.error(chalk.red('Internal error. No entry at index '+index));
+		console.error('Please report this error on the issue tracker.');
+		process.exit(3);
+	}
+	if (typeof target.kind === 'string'){
+		p = target.kind;
+	} 
 
-	const target = pool.hosts[index.toString()];
+	console.error('State: ',{
+		index: index,
+		size: pool.hosts.length
+	});
+	console.error('Record: ', target);
+
+
+	console.error(chalk.red('Plugin failure in discovery plugin "'+p+'": Missing or malformed SSH parameter.'));
+	console.error(chalk.red('The current state is displayed above. Please include this your report.'));
 
 	const childProcess = cp.spawn('ssh', [target.ssh], { stdio: 'inherit' });
 
