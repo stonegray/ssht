7e974b182ddd9bc20c50037a19ccdd118dc36aa0
Stone Gray <stone@stonegray.ca>
2020-12-30 01:30:48 -0500

    Disable reading version from plugins to resolve circular dependency

--- src/core/version.js
+++ src/core/version.js
@@ -16,6 +16,12 @@ export default async function version(options){
     console.log('');
     console.log('plugins:');
 
+    
+    /* I've removed the ability to get plugin versions for now, because they
+    create a circular dependency. It works fine, it's just hacked together */
+    console.log('\t[disabled, see ./src/core/version.js]');
+
+    /*
     const getPlugins = (await import('../discovery/pluginLoader.js'))
         .getPlugins;
 
@@ -36,6 +42,7 @@ export default async function version(options){
         }
         console.log(`\t${name} v${ver}`);
     }
+    */
 
     console.log('');
 
